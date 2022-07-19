623c0ba242021a672dc353ddd537c96576d40560
Ankit Sharma <sankit945@gmail.com>
2022-07-19 15:20:57 +0530

    added redis

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 import chokidar from 'chokidar';
 import { getDateTime } from './utils/date.util';
-import './services/redis.service';
 import { publishMessageToRedis } from './services/redis.service';
 
 const args = process.argv.slice(2);
 
 const watchDir = args[0];
+const containerName = args[1];
+if(!watchDir || !containerName) {
+    console.log('Please provide watch directory and container name');
+    process.exit(1);
+}
 
 console.log('Watching on  ', watchDir);
 
@@ -20,5 +24,5 @@ setInterval(() => {
 
 watcher.on('change', (path) => {
   console.log('File', path, 'has changed...');
-  publishMessageToRedis('container-file-changes', JSON.stringify({ path }));
+  publishMessageToRedis('container-file-changes', JSON.stringify({ path, containerName }));
 });
